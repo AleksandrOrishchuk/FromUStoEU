20c99b33890027c217341e58aab96ca03ef47e2c
AleksandrOrishchuk <alexbiohate@gmail.com>
2020-12-09 23:50:26 +0300

    Updated Converters.kt Updated database New conversions added

--- app/src/main/java/com/ssho/fromustoeu/binding_adapters/EditTextBindingAdapters.kt
+++ app/src/main/java/com/ssho/fromustoeu/binding_adapters/EditTextBindingAdapters.kt
@@ -1,10 +1,17 @@
 package com.ssho.fromustoeu.binding_adapters
 
 import android.text.TextWatcher
+import android.widget.AdapterView
 import android.widget.EditText
+import android.widget.Spinner
 import androidx.databinding.BindingAdapter
 
 @BindingAdapter("textChangedListener")
 fun bindTextWatcher(editText: EditText, textWatcher: TextWatcher) {
     editText.addTextChangedListener(textWatcher)
+}
+
+@BindingAdapter("spinnerListener")
+fun bindOnItemSelectedListener(spinner: Spinner, onItemSelectedListener: AdapterView.OnItemSelectedListener) {
+    spinner.onItemSelectedListener = onItemSelectedListener
 }
--- app/src/main/java/com/ssho/fromustoeu/converters/Converters.kt
+++ app/src/main/java/com/ssho/fromustoeu/converters/Converters.kt
@@ -7,18 +7,47 @@ class Converters {
                 return when (convertTargetName) {
                     "celsius" -> toCelsius(it)
                     "fahrenheits" -> toFahrenheits(it)
-                    "inches" -> toInch(it)
-                    "feet" -> toFeet(it)
                     "meters" -> toMeters(it)
-                    "square_feet" -> toSqFeet(it)
+                    "feet" -> toFeet(it)
                     "square_meters" -> toSqMeters(it)
-                    "miles_per_hour" -> toMPH(it)
+                    "square_feet" -> toSqFeet(it)
                     "kilometers_per_hour" -> toKMPH(it)
+                    "miles_per_hour" -> toMPH(it)
                     "kilograms" -> toKilograms(it)
                     "pounds" -> toPounds(it)
+                    "inches" -> toInch(it)
+                    "centimeters" -> toCentimeters(it)
+                    "litres" -> toLitres(it)
+                    "gallons" -> toGallons(it)
+                    "grams" -> toGrams(it)
+                    "ounces" -> toOunces(it)
                     else -> it
                 }
             }
+        }
+
+        private fun toOunces(input: Double): Double {
+            return input / 28.35
+        }
+
+        private fun toGrams(input: Double): Double {
+            return input * 28.35
+        }
+
+        private fun toGallons(input: Double): Double {
+            return input / 3.785
+        }
+
+        private fun toLitres(input: Double): Double {
+            return input * 3.785
+        }
+
+        private fun toCentimeters(input: Double): Double {
+            return input * 2.54
+        }
+
+        private fun toInch(input: Double): Double {
+            return input / 2.54
         }
 
         //input type is Kilograms
@@ -47,10 +76,6 @@ class Converters {
             return input / 10.764
         }
 
-        private fun toFahrenheits(input: Double): Double {
-            return input * 1.8 + 32
-        }
-
         private fun toSqFeet(input: Double): Double {
             return input * 10.764
         }
@@ -59,8 +84,8 @@ class Converters {
             return input * 3.281
         }
 
-        private fun toInch(input: Double): Double {
-            return input * 39.37
+        private fun toFahrenheits(input: Double): Double {
+            return input * 1.8 + 32
         }
 
         private fun toCelsius(input: Double): Double {
